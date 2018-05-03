474d3347ebcc09f585b48761e4224cb63c339255
NGAN PHAM <nganvpham@gmail.com>
2018-05-03 01:04:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber){
     let totalCost = total();
-    cart.splice(0);
+    cart =[];
     return`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
   } else {
     return "Sorry, we don't have a credit card on file for you.";
