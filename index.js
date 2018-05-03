1c1a74e35fd3780071ee572446173e3e19198e8f
NGAN PHAM <nganvpham@gmail.com>
2018-05-03 00:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber){
-    console.log(``)
-
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    let cart = [];
+    return cart;
+  } else {
+    console.log("")
   }
 }
