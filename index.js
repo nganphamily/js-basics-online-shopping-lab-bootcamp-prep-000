ef0624d9bb61ba2caa991eba927092d69f750276
NGAN PHAM <nganvpham@gmail.com>
2018-05-03 00:10:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,6 +64,6 @@ function placeOrder(cardNumber) {
   if (cardNumber){
     return`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   } else {
-    console.log("Sorry, we don't have a credit card on file for you.");
+    return "Sorry, we don't have a credit card on file for you.";
   }
 }
